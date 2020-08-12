c99d7f798628fa2d206b55dde4813d841c5320d3
Alexandra Onegård <alexandra.onegard@dynabyte.se>
2020-08-12 10:17:21 +0200

    Small fix in tsconfig for packages with build in types

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,60 @@
 'use strict';
 
 const webpack = require('webpack');
-const dotenv = require('dotenv');
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const env = require('dotenv').config();
 
-module.exports = () => {
-  const env = dotenv.config().parsed;
-  const mode = env.NODE_ENV || 'development';
+const mode = process.env.NODE_ENV || 'development';
 
-  // reduce it to a object
-  const envKeys = Object.keys(env).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(env[next]);
-    return prev;
-  }, {});
+// reduce it to a object
+const envKeys = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+  return prev;
+}, {});
 
-  return {
-    mode,
-    devtool: mode === 'development' ? 'inline-source-map' : false,
-    entry: './src/index.tsx',
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    // Supported file loaders
-    module: {
-      rules: [
-        {
-          test: /\.(tsx?)|(jsx?)$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.ts', '.tsx', '.js', 'jsx', '*'],
-    },
-    plugins: [new webpack.DefinePlugin(envKeys)],
-  };
+const config = {
+  mode,
+  devtool: mode === 'development' ? 'inline-source-map' : false,
+  // Supported file loaders
+  module: {
+    rules: [
+      {
+        test: /\.(tsx?)|(jsx?)$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', 'jsx'],
+    modules: ['src', 'node_modules'],
+  },
+  plugins: [
+    new webpack.DefinePlugin(envKeys),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
 };
+
+const client = Object.assign({}, config, {
+  name: 'client',
+  target: 'web',
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist/public'),
+  },
+});
+
+const server = Object.assign({}, config, {
+  name: 'server',
+  target: 'node',
+  externals: [nodeExternals()],
+  entry: './src/server/index.tsx',
+  output: {
+    filename: 'server.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+});
+
+module.exports = [client, server];
